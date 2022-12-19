4635994e30d0fefb09623c41e4fd72e42388f02e
loganashall <logan.m.ashall@gmail.com>
2022-12-19 11:21:51 -0800

    Added water level observation and prediction

--- grab_Tides_data.py
+++ grab_Tides_data.py
@@ -13,6 +13,23 @@ import numpy as np
 import pandas as pd
 import requests
 
+
+
+
+#load up gui
+text = "Please select if you want to download Water Level Observations or Predictions."
+title = "DFO Station Download"
+#load gui for checkbox
+wlo = 0
+wlp = 0
+answer = easygui.choicebox(text, title, ["Water Level Observations","Water level Predictions"])
+if answer =="Water Level Observations":
+    wlo=1
+elif answer =="Water level Predictions":
+    wlp=1
+print(wlo)    
+
+
 #load up gui
 text = "Please enter in StationID, Start Year and End Year for data download. StationID can be found in csv in folder (Point Atkinson StationID is given as reference)."
 title = "DFO Station Download"
@@ -21,9 +38,8 @@ default_list = ["5cebf1de3d0f4a073c4bb94c","2000","2022"]
 output = easygui.multenterbox(text, title, input_list, default_list)
 print (f'{output[0]}, {output[1]}, {output[2]}')
 
-#provide stationID
-stationID =output[0]
 
+stationID =output[0]
 
 # retrieve all station metadata
 response = requests.get(f'https://api-iwls.dfo-mpo.gc.ca/api/v1/stations')
@@ -60,10 +76,15 @@ bdf.to_csv(f'{stationID}-benchmark_metadata.csv', index=False)
 
 # download all water level observations from station ID
 frames =[]
+
+
 for year in range(int(output[1]),int(output[2])+1):
     for month in range(1,13):
         print(f'{year:04}-{month:02}')
-        response = requests.get(f'https://api-iwls.dfo-mpo.gc.ca/api/v1/stations/{stationID}/data?time-series-code=wlo&from={year:04}-{month:02}-01T00%3A00%3A00Z&to={year:04}-{month:02}-01T12%3A00%3A00Z')
+        if wlo ==1:
+            response = requests.get(f'https://api-iwls.dfo-mpo.gc.ca/api/v1/stations/{stationID}/data?time-series-code=wlo&from={year:04}-{month:02}-01T00%3A00%3A00Z&to={year:04}-{month:02}-01T12%3A00%3A00Z')
+        elif wlp==1:
+            response = requests.get(f'https://api-iwls.dfo-mpo.gc.ca/api/v1/stations/{stationID}/data?time-series-code=wlp&from={year:04}-{month:02}-01T00%3A00%3A00Z&to={year:04}-{month:02}-01T12%3A00%3A00Z')
         response.raise_for_status()  # raises exception when not a 2xx response
         if (
             response.status_code != 204 and
@@ -76,8 +97,10 @@ for year in range(int(output[1]),int(output[2])+1):
         df = pd.DataFrame(dic)
         frames.append(df)
 df = pd.concat(frames)
-df.to_csv(f'{stationID}-{output[1]}-{output[2]}.csv', index=False) 
-
+if wlp==1:
+    df.to_csv(f'{stationID}-wlp-{output[1]}-{output[2]}.csv', index=False) 
+if wlo==1:
+    df.to_csv(f'{stationID}-wlo-{output[1]}-{output[2]}.csv', index=False) 
 
 """ tdf= tdf.loc[tdf['code']=='wlo']
 timeSeriesWLOID = (tdf['id'].iloc[:1]).to_string(index=False)
